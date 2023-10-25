4f24a966c026dec4f4d39c5372232a1f4482c9be
Mike <133109697+Napal7va@users.noreply.github.com>
2023-10-25 15:41:10 +0300

    js file with hashbang string

---
+++ hashbang-grammar.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// in the Script Goal
+'use strict';
+console.log(2*3);
+
+#!/usr/bin/env node
+// in the Module Goal
+export {};
+console.log(2*2);
+
+//Строка #!/usr/bin/env node вызывает исходный файл Node.js напрямую как исполняемый файл.
